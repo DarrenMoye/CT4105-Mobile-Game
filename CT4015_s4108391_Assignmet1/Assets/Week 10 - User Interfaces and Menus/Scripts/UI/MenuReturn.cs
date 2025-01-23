6ba4499180aabb44387abcd7581a77a19c008954
DarrenMoye <darrenmoye02@gmail.com>
2025-01-23 14:25:48 +0000

    Added file from Week 10

---
+++ CT4015_s4108391_Assignmet1/Assets/Week 10 - User Interfaces and Menus/Scripts/UI/MenuReturn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuReturn : MonoBehaviour
+{
+
+    public string MenuScene;
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene(MenuScene);
+    }
+}
