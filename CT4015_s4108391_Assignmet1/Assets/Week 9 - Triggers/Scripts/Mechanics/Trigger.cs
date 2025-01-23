72258ba1ea6a4e5f1e622e64e12bb5003e190dc7
DarrenMoye <darrenmoye02@gmail.com>
2025-01-23 14:23:36 +0000

    Added files from Week 9

---
+++ CT4015_s4108391_Assignmet1/Assets/Week 9 - Triggers/Scripts/Mechanics/Trigger.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+
+    [SerializeField] public GameObject gameObjectToTrigger;
+    [SerializeField] public string triggerAnimatorParamater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+   
+    }
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        
+        if( collision.gameObject.tag == "Player")
+        {
+            if( gameObjectToTrigger )
+            {
+
+                gameObjectToTrigger.GetComponent<Animator>().SetTrigger( triggerAnimatorParamater );
+
+            }
+
+        }      
+
+    }
+
+    private void OnTriggerStay2D( Collider2D collision )
+    {
+    
+        if( collision.gameObject.tag == "Player")
+        {
+
+            if( gameObjectToTrigger )
+            {
+                gameObjectToTrigger.GetComponent<Animator>().SetTrigger( triggerAnimatorParamater );
+
+            }
+
+        }
+
+    }
+
+    private void OnTriggerExit2D( Collider2D collision )
+    {
+        if( collision.gameObject.tag == "Player")
+        {
+            if( gameObjectToTrigger )
+            {
+
+                gameObjectToTrigger.GetComponent<Animator>().ResetTrigger( triggerAnimatorParamater ); 
+
+            }
+
+        }  
+
+    }
+
+     private void OnCollisionEnter2D( Collision2D collision )
+    {
+        if( collision.gameObject.tag == "Player")
+        {
+
+            if( gameObjectToTrigger )
+            {
+
+                gameObjectToTrigger.GetComponent<Animator>().SetTrigger( triggerAnimatorParamater );
+                
+            }
+
+        }
+
+    }
+
+    private void OnCollisionStay2D( Collision2D collision )
+    {
+        if( collision.gameObject.tag == "Player")
+        {
+            if( gameObjectToTrigger )
+            {
+
+                gameObjectToTrigger.GetComponent<Animator>().SetTrigger( triggerAnimatorParamater );
+
+            }
+
+        }
+
+    }
+
+    private void OnCollisionExit2D( Collision2D collision )
+    {
+                      
+        if( collision.gameObject.tag == "Player")
+        {
+
+            if( gameObjectToTrigger )
+            {
+
+                gameObjectToTrigger.GetComponent<Animator>().ResetTrigger( triggerAnimatorParamater ); 
+
+            }
+
+        }   
+
+    }
+
+}
